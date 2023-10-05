9aefd5b917b1ffd2f10bdc7cb065b85dafb39842
peszabolcs <perjesidev@gmail.com>
2023-10-05 20:06:16 +0200

    Add eraser

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,76 @@
+import React, { useState } from "react";
 import "./App.css";
 import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
 import GridSize from "./Grid";
-import { useState } from "react";
 
 function App() {
   const [gridSize, setGridSize] = useState(121);
-  const handleInputChange = () => {
-    setGridSize(event.target.value);
+  const [currentColor, setCurrentColor] = useState("#000");
+  const [penMode, setPenMode] = useState("black");
+  const [activeButton, setActiveButton] = useState("black");
+  const [inputValue, setInputValue] = useState(121); // Initialize with 121
+  const [showMessage, setShowMessage] = useState(false); // State for showing the message
+
+  const handleInputChange = (event) => {
+    const newValue = parseInt(event.target.value, 10);
+    setInputValue(newValue);
+    setGridSize(newValue);
+
+    // Check if the input value is 1005 to show the message
+    if (newValue === 1005) {
+      setShowMessage(true);
+    } else {
+      setShowMessage(false);
+    }
+  };
+
+  const handlePenButtonClick = (mode) => {
+    setPenMode(mode);
+    if (mode === "eraser") {
+      setCurrentColor("#fff");
+    } else {
+      setCurrentColor("#000");
+    }
+
+    setActiveButton(mode);
   };
 
   return (
-    <>
-      <div className="appContainer">
-        <div className="container">
+    <div className="appContainer">
+      <div className="container">
+        <div className="sketch-left">
+          <div className="outer">
+            <div id="header">Etch-a-Sketch!</div>
+            <div className="sketch-content">
+              <GridSize gridSize={gridSize} currentColor={currentColor} />
+            </div>
+          </div>
+        </div>
+        <div className="pens-buttons-right">
           <div className="pens">
             <h1>Pens</h1>
             <div className="penButtons">
               <Button
                 variant="primary"
-                className="penButton"
+                className={`penButton ${
+                  activeButton === "black" ? "active" : ""
+                }`}
                 id="blackPenButton"
+                onClick={() => handlePenButtonClick("black")}
               >
                 Black
               </Button>
-              <br />
               <Button
                 variant="primary"
-                className="penButton"
-                id="rainbowPenButton"
+                className={`penButton ${
+                  activeButton === "eraser" ? "active" : ""
+                }`}
+                id="eraserButton"
+                onClick={() => handlePenButtonClick("eraser")}
               >
-                Rainbow
-              </Button>
-              <br />
-              <Button variant="primary" className="penButton" id="eraserButton">
                 Eraser
               </Button>
-              <br />
-            </div>
-          </div>
-          <div className="pen-image"></div>
-          <div className="sketch">
-            <div className="outer">
-              <div id="header">Etch-a-Sketch!</div>
-              <div className="sketch-content">
-                <GridSize gridSize={gridSize} />
-              </div>
             </div>
           </div>
           <div className="gridChanger">
@@ -53,16 +78,21 @@ function App() {
             <p>Adjust the grid size (only between 121 and 1024!)</p>
             <input
               type="number"
-              value={gridSize}
+              value={inputValue}
               onChange={handleInputChange}
               id="gridInput"
               min="121"
               max="1024"
             />
+            {showMessage && (
+              <p id="specialThanks">
+                Special thanks for my one and only Bodzsi &lt;33
+              </p>
+            )}
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
--- src/Grid.jsx
+++ src/Grid.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 import "./Grid.css";
 
-function GridSize({ gridSize }) {
+function GridSize({ gridSize, currentColor }) {
   const [numCols, setNumCols] = useState(1);
   const [numRows, setNumRows] = useState(1);
-  const [blackCells, setBlackCells] = useState([]);
+  const [coloredCells, setColoredCells] = useState([]);
   const [isDrawing, setIsDrawing] = useState(false);
 
   useEffect(() => {
@@ -17,9 +17,7 @@ function GridSize({ gridSize }) {
 
       document.documentElement.style.setProperty("--num-cols", cols);
       document.documentElement.style.setProperty("--num-rows", rows);
-
-      // Reset blackCells when gridSize changes
-      setBlackCells([]);
+      setColoredCells([]);
     }
   }, [gridSize]);
 
@@ -34,32 +32,55 @@ function GridSize({ gridSize }) {
 
   const handleGridCellHover = (index) => {
     if (isDrawing) {
-      if (!blackCells.includes(index)) {
-        setBlackCells([...blackCells, index]);
+      if (currentColor === "#fff") {
+        // If the current color is white (eraser), remove the cell color
+        setColoredCells((prevColoredCells) =>
+          prevColoredCells.filter((cellIndex) => cellIndex !== index)
+        );
+      } else {
+        // Otherwise, color the cell with the current pen color
+        setColoredCells((prevColoredCells) =>
+          prevColoredCells.includes(index)
+            ? prevColoredCells
+            : [...prevColoredCells, index]
+        );
       }
     }
   };
 
   const handleGridCellClick = (index) => {
-    if (!blackCells.includes(index)) {
-      setBlackCells([...blackCells, index]);
+    if (currentColor === "#fff") {
+      // If the current color is white (eraser), remove the cell color
+      setColoredCells((prevColoredCells) =>
+        prevColoredCells.filter((cellIndex) => cellIndex !== index)
+      );
+    } else {
+      // Otherwise, color the cell with the current pen color
+      setColoredCells((prevColoredCells) =>
+        prevColoredCells.includes(index)
+          ? prevColoredCells
+          : [...prevColoredCells, index]
+      );
     }
   };
 
-  const isCellBlack = (index) => blackCells.includes(index);
+  const isCellColored = (index) => coloredCells.includes(index);
 
   const divElements =
     gridSize >= 121 && gridSize <= 1024
       ? Array.from({ length: gridSize }, (_, index) => (
           <div
             className={`grid-cell ${
-              isCellBlack(index) ? "grid-cell-hover" : ""
+              isCellColored(index) ? "grid-cell-hover" : ""
             }`}
             key={index}
             onMouseDown={() => handleGridCellMouseDown(index)}
             onMouseUp={handleGridCellMouseUp}
             onMouseEnter={() => handleGridCellHover(index)}
             onClick={() => handleGridCellClick(index)}
+            style={{
+              backgroundColor: isCellColored(index) ? currentColor : "",
+            }}
           ></div>
         ))
       : null;
